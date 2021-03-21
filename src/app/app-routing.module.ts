ae166a0b9ca71f5bd795ad8e81039de91d6082f6
[Francisco Silva] <[francisco.r.e.silva@tecnico.ulisboa.pt]>
2021-03-21 12:23:40 +0000

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteDetailsComponent } from './pages/note-details/note-details.component';
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainLayoutComponent, children: [
+    { path: '', component: NotesListComponent },
+    /** path: 'new' must be above ':id' path
+     * Angular resolves routes from top to bottom
+     * If the ':id' route is above the 'new' route and a request is make to '/new'
+     * Angular will match the ':id' route and treat the 'new' as the id parameter
+    */
+    { path: 'new', component: NoteDetailsComponent},
+    { path: ':id', component: NoteDetailsComponent},
+  ] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
